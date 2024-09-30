bed3c5bd62dff1aac2ab537013a0eed7f4ba3dd3
John Sarga <johnsarga2@gmail.com>
2024-09-30 15:30:54 -0400

    added enumeration for pacemaker states

---
+++ Simulink/PacemakerMode.m
@@ -0,0 +1,12 @@
+classdef PacemakerMode < Simulink.IntEnumType
+    %Ennumeration of modes created here:
+    enumeration
+        Off(0)
+        AOO(1)
+        VOO(2)
+        AAI(3)
+        VVI(4)
+    end
+    
+end
+
